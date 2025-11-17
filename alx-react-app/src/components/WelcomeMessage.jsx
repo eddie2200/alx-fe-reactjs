1665e3c6cc9f40d2cf497144c84489867eb1e9d8
eddie2200 <eddiemartinnjau@gnail.com>
2025-11-17 17:18:37 +0300

    1. Modify JSX in a Pre-built React Component

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -5,7 +5,45 @@ function WelcomeMessage() {
             <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>I am learning about JSX!</p>
         </div>
-	<WelcomeMessage />
+	<WelcomeMessage /import { useState } from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      {/* Insert your new component */}
+      <WelcomeMessage />
+
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}>
+          count is {count}
+        </button>
+        <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
+>
     );
 }
 
