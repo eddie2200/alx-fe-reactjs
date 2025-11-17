e9dcd1002bf6c3453248d93fae40c6eedc6f0761
eddie2200 <eddiemartinnjau@gnail.com>
2025-11-18 00:56:53 +0300

    2. Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,20 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
+
+
 import UserProfile from './components/UserProfile';
 
 function App() {
